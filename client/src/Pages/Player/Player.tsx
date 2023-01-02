3d4bf61249f09eb5cc834ee632a782f903d39ba4
ssamkkim <ssamuel.kkim@gmail.com>
2023-01-01 23:18:43 -0800

    added default styling to player profile page

--- client/src/Pages/Player/Player.tsx
+++ client/src/Pages/Player/Player.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+import Navbar from '../../components/Navbar';
 
 const Player = () => {
-  return <div>Player</div>;
+  const { name } = useParams();
+  return (
+    <div className="bg-[#F2F2F2] h-screen overflow-y-scroll">
+      <Navbar />
+      <div className="flex justify-center">
+        <div className="w-1/2">{name}</div>
+      </div>
+    </div>
+  );
 };
 
 export default Player;
